6917394d031b3f38acba56f74bb7f41e1651f1d6
Devon Ulrich <dpulrich@gmail.com>
2020-08-28 15:23:48 -0400

    Made navbar functional
    Added auto-highlighting of active section with scrollspy, and used
    some JS to animate the scrolling behavior.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,31 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
+import $ from 'jquery';
+
 import './index.css';
+
+$(document).ready(function(){
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      var hash = this.hash;
+
+      var navOffset = $('nav').outerHeight();
+
+      // Using jQuery's animate() method to add smooth page scroll
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - navOffset
+      }, 800, function(){
+      });
+    } 
+  });
+});
 
 window.onload = AOS.init;
